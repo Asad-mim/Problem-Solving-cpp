e50e1e52aa0e19b8543ccd5f4bf7c6118e3708d8
Asad <129204973+Asad-mim@users.noreply.github.com>
2023-03-28 23:20:22 +0600

    Initial commit

---
+++ zooo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string n;
+	cin>>n;
+	int Z=0,O=0;
+	for(int i=0; i<n.size();i++)
+	{
+		if(n[i]=='Z' || n[i]=='z')
+		{
+			Z++;
+		}
+		else
+		{
+			O++;
+		}
+	}
+	if(Z*2==O)
+	{cout<<"Yes"<<endl;}
+	else
+	{cout<<"No"<<endl;}
+
+}
